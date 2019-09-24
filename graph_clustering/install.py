492fc9830d893f4fe1b4bdcc790ed0846989e060
Garreth <37878359+garrethmartin@users.noreply.github.com>
2019-09-24 16:50:52 -0700

    Update install.py

--- graph_clustering/install.py
+++ graph_clustering/install.py
@@ -18,3 +18,21 @@ print ('Installing Clustering')
 os.chdir(current_dir+'/code/dotnetcore/AgglomerativeClustering/')
 os.system('dotnet build --configuration Release')
 os.chdir(current_dir)
+
+print('Installing GNG')
+
+os.chdir(current_dir+'/code/dotnetcore/GNG')
+os.system('dotnet build --configuration Release')
+os.chdir(current_dir)
+
+print ('Installing ConnectedComponents')
+
+os.chdir(current_dir+'/code/dotnetcore/ConnComponents')
+os.system('dotnet build --configuration Release')
+os.chdir(current_dir)
+
+print ('Installing Clustering')
+
+os.chdir(current_dir+'/code/dotnetcore/AgglomerativeClustering/')
+os.system('dotnet build --configuration Release')
+os.chdir(current_dir)
